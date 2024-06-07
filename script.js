3fa14a8e7810c400f49fc35367587807d5bd6301
Kensei Maekawa <kensei_maekawa@wellness-technology.com>
2024-06-07 16:18:45 +0900

    ⚡️ 閉じる機能追加

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+// Youtube iframe API //
 
-
-
-
-// 2. This code loads the IFrame Player API code asynchronously.
+// 1. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
+// 2. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
+player = new YT.Player('player', {
     height: '360',
     width: '640',
     videoId: '-Km1W6HSbSM',
@@ -30,12 +28,12 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
+// 3. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
+// 4. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
 var done = false;
@@ -47,13 +45,15 @@ function onPlayerStateChange(event) {
     overlay.addEventListener('click', () => {
         console.log(overlay);
         player.unMute();
+        player.setOption('controls', 1);
     });
     done = true;
   }
 }
+//-- YouTube iframe API  --//
 
 
-
+// ページ読み込み→バナー表示まで
 const banner = document.querySelector('.banner');
 const bannerWrapper = document.querySelector('.banner-wrapper');
 
@@ -73,21 +73,65 @@ await mySetTimeout(banner, 'transition', '.5s');
 }
 displayBanner();
 
-overlay.addEventListener('click', () => {
+
+// 動画クリック→モーダル表示
+const overlayBg = document.querySelector('.overlay-bg');
+const modal = document.querySelector('.modal');
+
+overlay.addEventListener('click', (e) => {
     banner.style.opacity = '0';
     banner.style.transition = '.4s';
+
+    overlay.classList.add('overlay-bg');
+    const overlayBg = document.querySelector('.overlay-bg');
+    
     setTimeout(() => {
         banner.style.left = '50%';
         banner.style.top = '50%';    
         banner.style.width = "1000px";
-        banner.style.height = '50vh';
+        banner.style.height = '60vh';
         banner.style.padding = '100px';
         banner.style.background = '#fff';
         banner.style.transform = 'translate(-50%,-50%)';
         banner.style.boxShadow = 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px';
-        banner.style.borderRadius = '10px';
+        banner.style.borderRadius = '15px';
+
+        overlayBg.style.width = '100vw';
+        overlayBg.style.height = '100vh';
+        overlayBg.style.backgroundColor = '#000';
+        overlayBg.style.zIndex = '100';
     }, 600);
     setTimeout(() => {
         banner.style.opacity = '1';
+        overlayBg.style.opacity = '0.55';
+        overlayBg.style.transition = '.4s';
+        overlayBg.classList.remove('overlay');
     }, 1000);
+
+    overlayBg.addEventListener('click', () => {
+        modal.style.opacity = '0';
+        modal.style.transition = '.4s';
+        setTimeout(() => {
+          modal.style.display = 'none';
+        }, 400);
     });
+    }, {once: true});
+
+
+
+
+
+modal.addEventListener('click', (event) => {
+  if(event.target.closest(banner) === null) {
+    alert('外側をクリックされました。');
+  }
+});
+
+const closeBtn = document.querySelector('.player-close');
+closeBtn.addEventListener('click', () => {
+      modal.style.opacity = '0';
+      modal.style.transition = '.4s';
+      setTimeout(() => {
+        modal.style.display = 'none';
+      }, 400);
+});
